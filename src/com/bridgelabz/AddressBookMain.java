41cdc30e44de7226066c47e81d5f5b81d79eaa85
ajay812998 <ajay812998@gmail.com>
2021-01-18 22:44:18 +0530

    Added : Welcome to Address Book Problem

---
+++ src/com/bridgelabz/AddressBookMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+import java.util.*;
+
+public class AddressBookMain {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program");
+	}
+}
